0a9fa7b328d3fba6bc35836ed25f48fca26d9498
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-09-08 10:36:30 +0700

    update : assignment #3

--- assignment_3/17_0077_0107.ino
+++ assignment_3/17_0077_0107.ino
@@ -1,16 +1,17 @@
-int num_array[10][7] = {{0, 1, 1, 0, 0, 0, 0},  // 1
-                        {1, 1, 0, 1, 1, 0, 1},  // 2
-                        {1, 1, 1, 1, 0, 0, 1},  // 3
-                        {0, 1, 1, 0, 0, 1, 1},  // 4
-                        {1, 0, 1, 1, 0, 1, 1},  // 5
-                        {1, 0, 1, 1, 1, 1, 1},  // 6
-                        {1, 1, 1, 0, 0, 0, 0},  // 7
-                        {1, 1, 1, 1, 1, 1, 1},  // 8
-                        {1, 1, 1, 1, 0, 1, 1}}; // 9
+int new_num_array[10][7] = {{1, 0, 0, 1, 1, 1, 1}, // 0
+                            {0, 0, 1, 0, 0, 1, 0}, // 21
+                            {0, 0, 0, 0, 1, 1, 0}, // 3
+                            {1, 0, 0, 1, 1, 0, 0}, // 4
+                            {0, 1, 0, 0, 1, 0, 0}, // 5
+                            {0, 1, 0, 0, 0, 0, 0}, // 6
+                            {0, 0, 0, 1, 1, 1, 1}, // 7
+                            {0, 0, 0, 0, 0, 0, 0}, // 8
+                            {0, 0, 0, 0, 1, 0, 0},
+                            {1, 1, 1, 1, 1, 1, 1}}; // 9
 
-int check_array[3][7] = {{1, 1, 1, 1, 1, 1, 0},  // 0
-                         {1, 0, 1, 1, 1, 1, 0},  // G
-                         {0, 0, 0, 1, 1, 1, 0}}; // L
+int new_check_array[3][7] = {{0, 0, 0, 0, 0, 0, 1},  // 1
+                             {0, 1, 0, 0, 0, 0, 1},  // G
+                             {1, 1, 1, 0, 0, 0, 1}}; // L
 
 int currentNumber = 0;
 int button[2] = {13, 12};
@@ -38,6 +39,8 @@ void setup()
   randomNumber = random(1, 10);
   Serial.begin(9600);
   Serial.println(randomNumber);
+  number(currentNumber);
+  Serial.println(currentNumber);
 }
 
 int debounce(int i)
@@ -65,7 +68,7 @@ void number(int num)
   int pin = 2;
   for (int i = 0; i < 7; i++)
   {
-    digitalWrite(pin, num_array[num][i]);
+    digitalWrite(pin, (new_num_array[num][i]));
     pin++;
   }
 }
@@ -75,24 +78,27 @@ void check(int num)
   int pin = 2;
   for (int i = 0; i < 7; i++)
   {
-    digitalWrite(pin, check_array[num][i]);
+    digitalWrite(pin, (new_check_array[num][i]));
     pin++;
   }
 }
 
 void loop()
 {
+
   if (debounce(0))
   {
+
     if (!buttonState[0])
     {
       currentNumber++;
-      if (currentNumber == 10)
+      if (currentNumber == 9)
       {
-        currentNumber = 1;
+        currentNumber = 0;
       }
-      number(currentNumber - 1);
+      delay(500);
       Serial.println(currentNumber);
+      number(currentNumber);
     }
   }
 
@@ -100,20 +106,23 @@ void loop()
   {
     if (!buttonState[1])
     {
-      if (currentNumber == randomNumber)
+      if (currentNumber + 1 == randomNumber)
       {
+        delay(500);
         check(0);
         Serial.println("Equal");
         randomNumber = random(1, 10);
         Serial.println(randomNumber);
       }
-      else if (currentNumber < randomNumber)
+      else if (currentNumber + 1 < randomNumber)
       {
+        delay(500);
         check(2);
         Serial.println("Less than");
       }
-      else if (currentNumber > randomNumber)
+      else if (currentNumber + 1 > randomNumber)
       {
+        delay(500);
         check(1);
         Serial.println("More than");
       }
