77ef2e8d4f894ce2523d5a483200342cfa73c7ed
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-10-27 11:49:06 +0700

    update : assignment #6

--- assignment_6/master.ino
+++ assignment_6/master.ino
@@ -1,28 +1,158 @@
 #include <Wire.h>
+#define BOARD1 1
+#define BOARD2 2
+#define BOARD3 3
+#define BOARD4 4
 
-#define SLAVE_1 1
-#define SLAVE_2 2
+bool debug = 1;
 
-char text[] = "HELLO1";
-  
-int count = 0, slaveState;
+int currentBoard = BOARD1;
+int nextBoard = BOARD1;
+int numBoard = 3;
+char receiveMessage[64] = "\0";
+String serialInput;
+String message;
+String token;
+int i = 0;
+long time = 0;
+bool haveReceived = 0;
+int n = 0;
+int checkMessage = 0;
+int state = 0;
 
-void setup() {
-	
+void sendToken();
+
+void setup()
+{
+  Wire.begin(currentBoard);
+  Wire.onReceive(receiveEvent);
   Serial.begin(9600);
-  Wire.begin();
+  sendToken();
 }
 
-void loop() {
-  slaveState = count % 2 == 0 ? SLAVE_1 : SLAVE_2;
-  Wire.beginTransmission(slaveState);
-  Wire.write(text);
+void createToken()
+{
+  if (checkMessage == 2 && state == 0)
+  {
+    token = "Token#1#";
+    token += n;
+    token += "#";
+    token += message;
+    n = 0;
+    checkMessage = 0;
+    message = "";
+  }
+  else if (state == 1)
+  {
+    token = receiveMessage;
+  }
+  else
+  {
+    token = "Token#0#";
+  }
+  if (debug)
+  {
+    Serial.print("Send Token : ");
+    Serial.println(token);
+  }
+}
+
+bool cooldown()
+{
+  if (millis() - time > 200)
+  {
+    haveReceived = 0;
+    return 1;
+  }
+  return 0;
+}
+
+void sendToken()
+{
+  nextBoard %= numBoard;
+  nextBoard += 1;
+  if (nextBoard == currentBoard)
+  {
+    ++nextBoard;
+  }
+  createToken();
+  Wire.beginTransmission(nextBoard);
+  Wire.write(&token[0]);
   Wire.endTransmission();
-  Serial.println(text);
-  int len = Wire.requestFrom(slaveState, 6);
-  count++;
-  for(int i = 0; i < len; i++){
-  	text[i] = Wire.read();
-  } 
-  delay(1000);
+}
+
+void printMessage()
+{
+  for (int j = 10; j < i; ++j)
+  {
+    Serial.print(receiveMessage[j]);
+  }
+}
+
+void receiveEvent(int lenByte)
+{
+  for (i = 0; i < lenByte; i++)
+  {
+    receiveMessage[i] = Wire.read();
+  }
+  receiveMessage[i] = '\0';
+  if (debug)
+  {
+    Serial.print("Receive : ");
+    Serial.println(receiveMessage);
+  }
+  state = receiveMessage[6] - '0';
+  if (receiveMessage[8] - '0' == currentBoard)
+  {
+    state = 0;
+    printMessage();
+  }
+  time = millis();
+  haveReceived = 1;
+}
+
+void loop()
+{
+  while (Serial.available())
+  {
+    serialInput = Serial.readString();
+
+    if (checkMessage == 0)
+    {
+      n = serialInput.toInt();
+      if (n > numBoard || n <= 0)
+      {
+        Serial.println("Invalid Input Board");
+        Serial.println("Available Board : 1 2 3 4");
+        n = 0;
+      }
+      else
+      {
+        checkMessage = 1;
+      }
+    }
+    else if (checkMessage == 1)
+    {
+      message = serialInput;
+      checkMessage = 2;
+      if (debug)
+      {
+        Serial.print("Create Token : Token#1#");
+        Serial.print(n);
+        Serial.print("#");
+        Serial.println(message);
+      }
+    }
+    else
+    {
+      Serial.println("Can't queue more than 1 message");
+    }
+  }
+  if (haveReceived)
+  {
+    if (cooldown())
+    {
+      sendToken();
+    }
+  }
 }
--- assignment_6/slave_2.ino
+++ assignment_6/slave_2.ino
@@ -1,30 +1,145 @@
 #include <Wire.h>
+#define BOARD1 1
+#define BOARD2 2
+#define BOARD3 3
+#define BOARD4 4
 
-#define SLAVE_2 2
+int debug = 1;
+int currentBoard = BOARD3;
+int nextBoard = BOARD1;
+int numBoard = 3;
+char receiveMessage[64] = "\0";
+String serialInput;
+String message;
+String token;
+int i = 0;
+long time = 0;
+bool haveReceived = 0;
+int n = 0;
+int checkMessage = 0;
+int state = 0;
 
-char text[7];
-
-void setup() {
-	
-  Wire.begin(SLAVE_2);
-  Serial.begin(9600);
+void setup()
+{
+  Wire.begin(currentBoard);
   Wire.onReceive(receiveEvent);
-  Wire.onRequest(requestEvent);
+  Serial.begin(9600);
 }
 
-void loop() {}
-
-void receiveEvent(int byte) {
-  for(int i = 0; i < byte; i++){
-  	text[i] = Wire.read();
+bool cooldown()
+{
+  if (millis() - time > 200)
+  {
+    haveReceived = 0;
+    return 1;
   }
-  Serial.println(text);
+  return 0;
 }
 
-void requestEvent() {
-  text[5]++;
-  if (text[5] == ':') {
-  	text[5] = '1';
+void createToken()
+{
+  if (checkMessage == 2 && state == 0)
+  {
+    token = "Token#1#";
+    token += n;
+    token += "#";
+    token += message;
+    n = 0;
+    checkMessage = 0;
+    message = "";
   }
-  Wire.write(text);
+  else if (state == 1)
+  {
+    token = receiveMessage;
+  }
+  else
+  {
+    token = "Token#0#";
+  }
+  if (debug)
+  {
+    Serial.print("Send Token : ");
+    Serial.println(token);
+  }
+}
+
+void sendToken()
+{
+  Wire.beginTransmission(nextBoard);
+  createToken();
+  Wire.write(&token[0]);
+  Wire.endTransmission();
+}
+
+void printMessage()
+{
+  for (int j = 10; j < i; ++j)
+  {
+    Serial.print(receiveMessage[j]);
+  }
+}
+
+void receiveEvent(int lenByte)
+{
+  for (i = 0; i < lenByte; i++)
+  {
+    receiveMessage[i] = Wire.read();
+  }
+  receiveMessage[i] = '\0';
+  if (debug)
+  {
+    Serial.print("Receive : ");
+    Serial.println(receiveMessage);
+  }
+  state = receiveMessage[6] - '0';
+  if (receiveMessage[8] - '0' == currentBoard)
+  {
+    state = 0;
+    printMessage();
+  }
+  time = millis();
+  haveReceived = 1;
+}
+
+void loop()
+{
+  while (Serial.available())
+  {
+    serialInput = Serial.readString();
+
+    if (checkMessage == 0)
+    {
+      n = serialInput.toInt();
+      if (n > numBoard || n <= 0)
+      {
+        Serial.println("Invalid Input Board");
+        Serial.println("Available Board : 1 2 3");
+        n = 0;
+      }
+      else
+      {
+        checkMessage = 1;
+      }
+    }
+    else if (checkMessage == 1)
+    {
+      message = serialInput;
+      checkMessage = 2;
+      Serial.print("Create Token : Token#1#");
+      Serial.print(n);
+      Serial.print("#");
+      Serial.println(message);
+    }
+    else
+    {
+      Serial.println("Can't queue more than 1 message");
+    }
+  }
+  if (haveReceived)
+  {
+    if (cooldown())
+    {
+      sendToken();
+    }
+  }
 }
